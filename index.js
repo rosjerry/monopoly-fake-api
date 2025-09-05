101159aa77a361081bf01b5fb4f34a4530c270f2
rosjerry <rostomi1306@gmail.com>
2025-09-05 18:41:30 +0400

    feat: add very minimal setup for dice results

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// /dicer route that returns array of 2 random numbers (1-6)
+app.get("/dice", (req, res) => {
+  const dice1 = Math.floor(Math.random() * 6) + 1;
+  const dice2 = Math.floor(Math.random() * 6) + 1;
+
+  res.json([dice1, dice2]);
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
