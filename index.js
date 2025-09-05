0ff77d70f137f0c74434566841df671dbd5cf5cf
rosjerry <rostomi1306@gmail.com>
2025-09-05 18:43:44 +0400

    feat: add cors to enable requests from anywhere

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Enable CORS from everywhere
+app.use(cors());
 
 // /dicer route that returns array of 2 random numbers (1-6)
 app.get("/dice", (req, res) => {
